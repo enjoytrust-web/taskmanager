62c5ae5074c426db812ee1733ed45d9549288d1f
nakamura enjoy <nakamura@enjoytrust.net>
2017-08-08 23:01:33 +0900

    フォントを追加しました

--- tpl_taskmanager_v1.1/index.php
+++ tpl_taskmanager_v1.1/index.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
 
-
 <div class="subheader">
   <?php $user = wp_get_current_user(); ?>
     <p class="admin-image"><?php echo get_avatar($user->get('ID'), 128); // アバター取得 ?></p>
@@ -10,73 +9,102 @@
 
 <div class="l-wrapper">
   <div class="l-full">
-    <div class="l-grid-12">
-      <a href="<?php the_permalink($post->ID);?>" class="task-list-card">
-        <p class="task-title">フライヤー制作</p>
-        <p class="task-label">クライアント</p>
-        <p class="task-lead">GIRAFFE JAPAN
+
+    <div class="l-grid-4">
+      <div class="task-list-card">
+        <p class="task-info-date">
+        <p class="index">Order Date - 受理日</p>
+        <p class="lead lead-minimum">2017/08/01</p>
+        <p class="index">Order Title - 案件名</p>
+        <p class="lead lead-title">フライヤー制作</p>
+        <p class="index">Client - クライアント</p>
+        <p class="lead lead-large">GIRAFFE JAPAN
           <span class="keishou">&nbsp;様</span>
         </p>
-        <p class="task-label">制作物の種類</p>
-        <p class="task-lead">フライヤー制作</p>
-        <p class="task-label">担当者</p>
-        <p class="task-lead">中村</p>
-        <p class="task-label">作業ステータス</p>
-        <p class="task-lead">未着手</p>
-      </a>
+        <p class="index">Deadline - 納期</p>
+        <p class="lead lead-large">2017/08/07</p>
+        <p class="index">Order Info - 制作物の種類</p>
+        <p class="lead lead-small">フライヤー / ポスター</p>
+        <p class="index">Charger - 作業担当者</p>
+        <p class="lead lead-label">中村</p>
+        <p class="index">Status - 作業ステータス</p>
+        <p class="lead lead-label">未着手</p>
+        <div class="l-row button-group">
+          <div class="l-grid-6">
+            <a href="" class="tasklist-edit">このタスクを更新する</a>
+          </div>
+          <div class="l-grid-6">
+            <a href="" class="tasklist-info">詳細を確認する</a>
+          </div>
+        </div>
+
+        <div class="card-corner graphic"></div>
+      </div>
     </div>
-    <div class="l-grid-12">
-      <a href="<?php the_permalink($post->ID);?>" class="task-list-card">
-        <p class="task-title">フライヤー制作</p>
-        <p class="task-label">クライアント</p>
-        <p class="task-lead">GIRAFFE JAPAN
+
+    <div class="l-grid-4">
+      <div class="task-list-card">
+        <p class="task-info-date">
+        <p class="index">Order Date - 受理日</p>
+        <p class="lead lead-minimum">2017/08/01</p>
+        <p class="index">Order Title - 案件名</p>
+        <p class="lead lead-title">フライヤー制作</p>
+        <p class="index">Client - クライアント</p>
+        <p class="lead lead-large">GIRAFFE JAPAN
           <span class="keishou">&nbsp;様</span>
         </p>
-        <p class="task-label">制作物の種類</p>
-        <p class="task-lead">フライヤー制作</p>
-        <p class="task-label">担当者</p>
-        <p class="task-lead">中村</p>
-      </a>
+        <p class="index">Deadline - 納期</p>
+        <p class="lead lead-large">2017/08/07</p>
+        <p class="index">Order Info - 制作物の種類</p>
+        <p class="lead lead-small">フライヤー / ポスター</p>
+        <p class="index">Charger - 作業担当者</p>
+        <p class="lead lead-label">中村</p>
+        <p class="index">Status - 作業ステータス</p>
+        <p class="lead lead-label">未着手</p>
+        <div class="l-row button-group">
+          <div class="l-grid-6">
+            <a href="" class="tasklist-edit">このタスクを更新する</a>
+          </div>
+          <div class="l-grid-6">
+            <a href="" class="tasklist-info">詳細を確認する</a>
+          </div>
+        </div>
+
+        <div class="card-corner web"></div>
+      </div>
     </div>
-    <div class="l-grid-12">
-      <a href="<?php the_permalink($post->ID);?>" class="task-list-card">
-        <p class="task-title">フライヤー制作</p>
-        <p class="task-label">クライアント</p>
-        <p class="task-lead">GIRAFFE JAPAN
+
+    <div class="l-grid-4">
+      <div class="task-list-card">
+        <p class="task-info-date">
+        <p class="index">Order Date - 受理日</p>
+        <p class="lead lead-minimum">2017/08/01</p>
+        <p class="index">Order Title - 案件名</p>
+        <p class="lead lead-title">フライヤー制作</p>
+        <p class="index">Client - クライアント</p>
+        <p class="lead lead-large">GIRAFFE JAPAN
           <span class="keishou">&nbsp;様</span>
         </p>
-        <p class="task-label">制作物の種類</p>
-        <p class="task-lead">フライヤー制作</p>
-        <p class="task-label">担当者</p>
-        <p class="task-lead">中村</p>
-      </a>
+        <p class="index">Deadline - 納期</p>
+        <p class="lead lead-large">2017/08/07</p>
+        <p class="index">Order Info - 制作物の種類</p>
+        <p class="lead lead-small">フライヤー / ポスター</p>
+        <p class="index">Charger - 作業担当者</p>
+        <p class="lead lead-label">中村</p>
+        <p class="index">Status - 作業ステータス</p>
+        <p class="lead lead-label">未着手</p>
+        <div class="l-row button-group">
+          <div class="l-grid-6">
+            <a href="" class="tasklist-edit">このタスクを更新する</a>
+          </div>
+          <div class="l-grid-6">
+            <a href="" class="tasklist-info">詳細を確認する</a>
+          </div>
+        </div>
+        <div class="card-corner movie"></div>
+      </div>
     </div>
-    <div class="l-grid-12">
-      <a href="<?php the_permalink($post->ID);?>" class="task-list-card">
-        <p class="task-title">フライヤー制作</p>
-        <p class="task-label">クライアント</p>
-        <p class="task-lead">GIRAFFE JAPAN
-          <span class="keishou">&nbsp;様</span>
-        </p>
-        <p class="task-label">制作物の種類</p>
-        <p class="task-lead">フライヤー制作</p>
-        <p class="task-label">担当者</p>
-        <p class="task-lead">中村</p>
-      </a>
-    </div>
-    <div class="l-grid-12">
-      <a href="<?php the_permalink($post->ID);?>" class="task-list-card">
-        <p class="task-title">フライヤー制作</p>
-        <p class="task-label">クライアント</p>
-        <p class="task-lead">GIRAFFE JAPAN
-          <span class="keishou">&nbsp;様</span>
-        </p>
-        <p class="task-label">制作物の種類</p>
-        <p class="task-lead">フライヤー制作</p>
-        <p class="task-label">担当者</p>
-        <p class="task-lead">中村</p>
-      </a>
-    </div>
+
   </div>
 </div>
 
